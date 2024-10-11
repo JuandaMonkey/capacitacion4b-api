1d13a5d2efaab256d23f5e68ed57a58111657a0e
JuandaMonkey <juandanieldecide@gmail.com>
2024-10-10 20:35:46 -0600

    capacitacion4b-api

---
+++ capacitacion4b-api.Models/userModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capacitacion4b_api.Models
+{
+
+    internal class userModel
+    {
+
+        /* idUsuario */
+        public int idUsuario { get; set; }
+        /* nombresUsuario */
+        public string? nombreUsuario { get; set; }
+        /* usuarioUsuario */
+        public string? usuarioUsuario { get; set; }
+        /* contrasenaUsuario*/
+        public string? contrasenaUsuario { get; set; }
+
+    }
+
+}
